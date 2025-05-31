b2d2558fa325647c2079ec27a74b25fb2121c638
LShepelev <Lev.Shepelev@aton.ru>
2025-05-31 13:11:36 +0300

    Init DVC

--- stocks_prediction/visualization.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-def plot_results(y_test, y_pred):
-    plt.figure(figsize=(10, 5))
-    plt.plot(y_test, label="Actual")
-    plt.plot(y_pred, label="Predicted")
-    plt.legend()
-    plt.savefig("output.png")
